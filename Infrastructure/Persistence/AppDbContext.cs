c49aad11fc35f377cd3ade88e3fdf7632a8098ab
AndrexMed <giovanniandrex@gmail.com>
2023-12-28 10:11:35 -0500

    Capa Infrastructure

---
+++ Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,42 @@
+﻿using Application.Data;
+using Domain.Customer;
+using Domain.Primitives;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence
+{
+    public class AppDbContext : DbContext, IApplicationDbContext, IUnityOfWork
+    {
+        private readonly IPublisher _publisher;
+
+        public AppDbContext(IPublisher publisher, DbContextOptions options) : base(options)
+        {
+            _publisher = publisher ?? throw new ArgumentNullException(nameof(publisher));
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            var domainsEvent = ChangeTracker.Entries<AggregateRoot>()
+                .Select(x => x.Entity)
+                .Where(x => x.GetDomainEvents().Any())
+                .SelectMany(x => x.GetDomainEvents());
+
+            var result = await base.SaveChangesAsync(cancellationToken);
+
+            foreach (var domainEvent in domainsEvent)
+            {
+                await _publisher.Publish(domainEvent, cancellationToken);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Infrastructure/Persistence/Repositories/CustomerRepository.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Customer;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence.Repositories
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public CustomerRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext ?? throw new ArgumentNullException(nameof(appDbContext));
+        }
+
+        public async Task Add(Customer customer) => await _appDbContext.AddAsync(customer);
+
+        public async Task<Customer?> GetByIdAsync(CustomerId id) => await _appDbContext.Customers.SingleOrDefaultAsync(x => x.Id == id);
+    }
+}
