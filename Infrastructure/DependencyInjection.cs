83017f90f424de1929c71996b7f8f7496713ebfc
AndrexMed <giovanniandrex@gmail.com>
2023-12-28 14:38:16 -0500

    Migraciones

---
+++ Infrastructure/DependencyInjection.cs
@@ -0,0 +1,35 @@
+﻿using Application.Data;
+using Domain.Customer;
+using Domain.Primitives;
+using Infrastructure.Persistence;
+using Infrastructure.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddPersistence(configuration);
+            return services;
+        }
+
+        private static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("Database")));
+
+            services.AddScoped<IApplicationDbContext>(sp =>
+                sp.GetRequiredService<AppDbContext>());
+
+            services.AddScoped<IUnityOfWork>(sp =>
+                    sp.GetRequiredService<AppDbContext>());
+
+            services.AddScoped<ICustomerRepository, CustomerRepository>();
+
+            return services;
+        }
+    }
+}
---
+++ Web.Api/Extensions/MigrationExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Web.Api.Extensions
+{
+    public static class MigrationExtensions
+    {
+        public static void ApplyMigration(this WebApplication app)
+        {
+            using var scope = app.Services.CreateScope();
+
+            var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+            dbContext.Database.Migrate();
+        }
+    }
+}
